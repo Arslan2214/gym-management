baa0c6b5edec686c376d29c4a175260c5fd7a2cc
Arslan Ahmad <mycomputer2214@gmail.com>
2023-10-15 16:36:37 +0500

    Initializing Repo

---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+import { Box } from '@mui/material'
+
+import HeroBanner from '../components/HeroBanner';
+import SearchExcercise from '../components/SearchExcercise.js';
+import Excercises from '../components/Excercises';
+
+const Home = () => {
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExcercise />
+      <Excercises />
+    </Box>
+  )
+}
+
+export default Home
